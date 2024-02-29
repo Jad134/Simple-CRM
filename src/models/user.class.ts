3a4381681e641ca47070d42167a4f599a33f9a98
Jad El Nader <Jadelnader99@gmail.com>
2024-02-29 16:57:13 +0100

    Add email

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,6 +1,7 @@
 export class User {
     firstName!: string;
     lastName!: string;
+    email!: string;
     birthDate!: number;
     street!: String;
     zipCode!: number;
@@ -9,20 +10,23 @@ export class User {
     constructor(obj?: any) {
         this.firstName = obj ? obj.firstName : '';
         this.lastName = obj ? obj.lastName : '';
+        this.email = obj ? obj.email: '';
         this.birthDate = obj ? obj.birthDate : 0;
         this.street = obj ? obj.street : '';
         this.zipCode = obj ? obj.zipCode : '';
         this.city = obj ? obj.city : '';
+       
     }
 
-    toJSON(): any {
-        return {
-            firstName: this.firstName,
-            lastName: this.lastName,
-            birthDate: this.birthDate,
-            street: this.street,
-            zipCode: this.zipCode,
-            city: this.city
-        };
-    }
+    // toJSON(): any {
+    //     return {
+    //         firstName: this.firstName,
+    //         lastName: this.lastName,
+    //         email: this.email,
+    //         birthDate: this.birthDate,
+    //         street: this.street,
+    //         zipCode: this.zipCode,
+    //         city: this.city
+    //     };
+    // }
 }
